2002f1aa8d239bd35f7474aac1c4a82a1dd60226
aryaganteng <hadi.argaya@gmail.com>
2025-05-24 02:35:44 +0700

    Update Tampilan Home

--- app/src/main/java/id/co/bcaf/adapinjam/data/utils/RetrofitClient.kt
+++ app/src/main/java/id/co/bcaf/adapinjam/data/utils/RetrofitClient.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 //    private const val BASE_URL = "http://35.223.1.74/be/"
-    private const val BASE_URL = "https://21fe-120-188-32-150.ngrok-free.app/be/" // Ganti dengan URL API kamu
+    private const val BASE_URL = "https://5d70-120-188-33-177.ngrok-free.app/be/" // Ganti dengan URL API kamu
 
     private val client = OkHttpClient.Builder()
         .connectTimeout(60, TimeUnit.SECONDS) // Set connect timeout
